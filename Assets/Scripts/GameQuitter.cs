6adf3ae4f4ec6498165f4929b7d4b1e0f5702f53
akolybelnikov <a.kolybelnikov@gmail.com>
2020-09-18 18:34:44 +0200

    Set up the start menu.

---
+++ Assets/Scripts/GameQuitter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameQuitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(QuitGame);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStarter : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(StartGame);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
